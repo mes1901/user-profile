db9ea7861a144c297824516a21b8aec2e4a10523
eduard <motrii.edu@gmail.com>
2019-11-24 19:00:47 +0200

    change env credentials

--- config/env.example.php
+++ config/env.example.php
@@ -3,10 +3,10 @@
 declare(strict_types=1);
 
 $variables = [
-    'DB_HOST' => 'mysql',
+    'DB_HOST' => 'localhost',
     'DB_USERNAME' => 'root',
-    'DB_PASSWORD' => 'pass',
-    'DB_NAME' => 'db',
+    'DB_PASSWORD' => 'secret',
+    'DB_NAME' => 'db_name',
 ];
 
 foreach ($variables as $key => $value) {
